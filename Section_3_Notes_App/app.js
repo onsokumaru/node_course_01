9676fdc27ceec593e61d443b9898942e83c7c398
mike <11082686+onsokumaru@users.noreply.github.com>
2019-12-31 12:01:47 -0500

    added challege code

--- Section_3_Notes_App/app.js
+++ Section_3_Notes_App/app.js
@@ -1,3 +1,10 @@
  const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'My name is Mike.')
+// fs.writeFileSync('notes.txt', 'My name is Mike.')
+
+// challenge: append a message to the notes.txt file
+// 1. use appendFileSync to append to file
+// 2. run script
+// 3. check your work by opening file and viewing appended text
+
+fs.appendFileSync('notes.txt', "\nI currently live in St. Petersburg, Florida.")
