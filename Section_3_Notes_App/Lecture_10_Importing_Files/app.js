fb0332cdb24edb6f697341df3f58f92ea5387614
mike <11082686+onsokumaru@users.noreply.github.com>
2019-12-31 13:53:05 -0500

    added utils.js , added challenge instructions

---
+++ Section_3_Notes_App/Lecture_10_Importing_Files/app.js
@@ -0,0 +1,23 @@
+// challenge: define and use a function in a new file
+//
+// 1. create a new file called notes.js
+// 2. create getNotes function that return "you notes"
+// 3. export getNotes function
+// 4. from app.js, load in and call the function printing the message to the console
+
+
+
+
+
+
+
+
+// // const name = require('./utils.js')
+
+// // console.log(name)
+
+// const add = require('./utils.js')
+
+// const sum = add(4, -2)
+
+// console.log(sum)
