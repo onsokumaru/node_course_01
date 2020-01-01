05db65e172755814b0553f03d0a8daad51ffd8c5
mike <11082686+onsokumaru@users.noreply.github.com>
2020-01-01 14:52:25 -0500

    added code from lecture

--- Section_3_Notes_App/Notes_App/app.js
+++ Section_3_Notes_App/Notes_App/app.js
@@ -1,5 +1,14 @@
+const validator = require('validator')
 const displayNotes = require('./notes.js')
 
 const myNotes = displayNotes()
 
 console.log(myNotes)
+
+// console.log(validator.isEmail('mike@example.com'))
+// console.log(validator.isEmail('example.com'))
+
+console.log(validator.isURL('https://mead.io'))
+console.log(validator.isURL('https:/mead.io'))
+
+
